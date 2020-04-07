c346a639b6b390f7ec64e6adf03191bb16500e52
V tussenvoegsels Achternaam <jurgenvanderheiden@gmail.com>
2020-04-07 11:47:35 +0200

    Lecture 2 fix

--- lectures/2_two/main.ts
+++ lectures/2_two/main.ts
@@ -35,7 +35,7 @@ let transform_countainer_content_num_to_bool =
 }
 
 // i.e. map container
-let map_countainer = 
+let map_countainer1 = 
   function<a,b>(f:Fun<a,b>, c:Countainer<a>) 
   : Countainer<b> { 
   return { content:f.f(c.content), counter:c.counter }
@@ -43,7 +43,7 @@ let map_countainer =
 
 
 let c:Countainer<number> = { content:3, counter:0 }
-let l:Countainer<boolean> = map_countainer(isEven, c)
+let l:Countainer<boolean> = map_countainer1(isEven, c)
 
 //I.e. of two funcs in one
 // let map_F_G = function<a,b>(f:Fun<a,b>) => Fun<F_G<a>, F_G<b>> {
@@ -56,10 +56,37 @@ let l:Countainer<boolean> = map_countainer(isEven, c)
 //   return map_Countainer<Option<a>,Option<b>>(map_Option<a,b>(f)));
 // }
 
+//interface... changed the code. Is this correct?
+type map_countainer = <a,b>(f:Fun<a,b>, c:Countainer<a>) => Countainer<b>
+
+//not working... why?
 // let map_countainer = 
 //   function<a,b>(f:Fun<a,b>) : Fun<Countainer<a>, Countainer<b>> { 
 //   return Fun(c =>{ content:f.f(c.content), counter:c.counter })
 // }
+//c =>{ content:f.f(c.content), counter:c.counter }
+
+// let map_F<a,a>(id<a>()) = id<F<a>>()
+
+
+//option
+type Option<a> = { kind:"none" } | { kind:"some", value:a }
+
+function print(x:Option<number>) : string {
+  if (x.kind == "some")
+    return `the value is ${x.value}`
+  else 
+    return "there is no value"
+}
+
+let none = function<a>() : Option<a> { return { kind:"none" } }
+let some = function<a>(x:a) : Option<a> { 
+  return { kind:"some", value:x } }
+  
+let map_Option = function<a,b>(f:Fun<a,b>) : Fun<Option<a>,Option<b>> {
+    //not yet understand the code below
+    return Fun(x => x.kind == "none" ? none<b>() : some<b>(f.f(x.value))) 
+  }
 
 type List<a> = {
   kind: "Cons"
