b6544b8b27f2ac05d4336c29426a8f3896d71550
V tussenvoegsels Achternaam <jurgenvanderheiden@gmail.com>
2020-05-20 11:20:05 +0200

    Update

--- project/src/main.ts
+++ project/src/main.ts
@@ -6,10 +6,13 @@ import {dbTables} from './ORM/Database'
         - Include = works as an Union all, but the selected amount of columns set the amount of columns for the include table
                     this may result in empty columns or that not all columns are shown of the second list
         - Where = This is an union all if tableName doesn't exist in the selected columns than nothing will be shown. 
+        - OrderBy = currently kind of working
     @Note: 
         - one can only select once include and select (in one instance)
         - Table students has 2 'foreign keys', but one cannot do an INNER JOIN
         - one has to start with select
+    @todo:
+        - Orderby: changes: ASC 15,12,8,7 in 12,15,7,8 instead of 7,8,12,15
 */
 
 //just showing the SELECT
